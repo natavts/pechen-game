fb576005bf9d4e04459b1536f931b184099bb269
Александр Можаев <nadymorigin@gmail.com>
2020-02-18 15:27:51 +0400

    feat: ActionsAction

---
+++ packages/app/src/actions/ActionsAction.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import Telegraf from 'telegraf';
+import { IncomingMessage } from 'telegraf/typings/telegram-types'; // eslint-disable-line
+
+import Action, { ActionProps } from './Action'; // eslint-disable-line
+
+export class ActionsAction extends Action {
+  constructor(props: ActionProps) {
+    super(props);
+    this.name = 'ActionsAction';
+  }
+
+  private actionsButtons = Telegraf.Extra.markdown().markup(m => {
+    console.log({ m });
+    return m.keyboard(['🗡 Атаковать', '🛡 Защищаться', 'Назад']);
+  });
+
+  public test(message: IncomingMessage): boolean {
+    if (!message.text) return false;
+    return message.text.match(/Ходить/) != null;
+  }
+
+  public exec(message: IncomingMessage): void {
+    const chatId = message.from?.id;
+    if (!chatId) return;
+    this.bot.telegram.sendMessage(chatId, 'Выберите действие', this.actionsButtons); // refresh
+  }
+}
--- packages/app/src/actions/index.ts
+++ packages/app/src/actions/index.ts
@@ -2,3 +2,4 @@ export { PolundraAction } from './PolundraAction'; // eslint-disable-line
 export { StartGameAction } from './StartGameAction'; // eslint-disable-line
 export { JoinAction } from './JoinAction'; // eslint-disable-line
 export { WhoAmIAction } from './WhoAmIAction'; // eslint-disable-line
+export { ActionsAction } from './ActionsAction'; // eslint-disable-line
