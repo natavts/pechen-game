d622a6b3ae2be1452a4a8f59106dfb8ecf58ccc6
Александр Можаев <nadymorigin@gmail.com>
2020-02-20 19:29:38 +0400

    feat: imports actions

--- packages/app/src/actions/index.ts
+++ packages/app/src/actions/index.ts
@@ -3,3 +3,9 @@ export { StartGameAction } from './StartGameAction'; // eslint-disable-line
 export { JoinAction } from './JoinAction'; // eslint-disable-line
 export { WhoAmIAction } from './WhoAmIAction'; // eslint-disable-line
 export { ActionsAction } from './ActionsAction'; // eslint-disable-line
+export { InfoAction } from './InfoAction'; // eslint-disable-line
+export { UsersListAction } from './UsersListAction'; // eslint-disable-line
+export { CharactersListAction } from './CharactersListAction'; // eslint-disable-line
+export { AttackAction } from './AttackAction'; // eslint-disable-line
+export { DefenceAction } from './DefenceAction'; // eslint-disable-line
+// export { StatusAction } from './StatusAction'; // eslint-disable-line
