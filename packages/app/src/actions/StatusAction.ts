bb4f222aeca61a238fecc90655d3eb41873f0aee
Александр Можаев <nadymorigin@gmail.com>
2020-03-02 19:13:47 +0400

    feat: status action show all users

--- packages/app/src/actions/StatusAction.ts
+++ packages/app/src/actions/StatusAction.ts
@@ -2,12 +2,10 @@
 /* eslint-disable import/extensions */
 /* eslint-disable @typescript-eslint/camelcase */
 import { IncomingMessage } from 'telegraf/typings/telegram-types'; // eslint-disable-line
-import find from 'lodash/find';
-import join from 'lodash/join';
 
 import Action, { ActionProps } from './Action'; // eslint-disable-line
+import { getStatus } from '../game/utils';
 import { menuButtons } from '../buttons';
-import { CharacterData, ActionData, TurnType } from '../game';
 
 export class StatusAction extends Action {
   constructor(props: ActionProps) {
@@ -23,56 +21,6 @@ export class StatusAction extends Action {
   public exec(message: IncomingMessage): void {
     const userId = message.from?.id;
     if (!userId) return;
-    const { round } = this.gameRoom.game;
-    const username = message.from?.username;
-    const points = this.gameRoom.game.getPlayer(userId)?.points;
-
-    const attack = find(this.gameRoom.game.events, { type: TurnType.attack, data: { userId } });
-    const defence = find(this.gameRoom.game.events, { type: TurnType.defence, data: { userId } });
-
-    const opponentAttacks = this.gameRoom.game.events.filter(
-      event => event.type === 'attack' && (event.data as ActionData).opponentId === userId,
-    );
-    const opponentDefences = this.gameRoom.game.events.filter(
-      event => event.type === 'defence' && (event.data as ActionData).opponentId === userId,
-    );
-    const characters = this.gameRoom.game.events.filter(
-      event => event.type === 'character' && event.data.userId === userId,
-    );
-
-    let attackPlayer = '';
-    if (attack) {
-      attackPlayer = `@${this.gameRoom.game.getPlayer((attack.data as ActionData).opponentId)?.name}`;
-    }
-    let defencePlayer = '';
-    if (defence) {
-      defencePlayer = `@${this.gameRoom.game.getPlayer((defence.data as ActionData).opponentId)?.name}`;
-    }
-
-    let opponentAttacksList: string[] = [];
-    if (opponentAttacks) {
-      opponentAttacksList = opponentAttacks.map(item => `@${this.gameRoom.game.getPlayer(item.data.userId)?.name}`);
-    }
-    let opponentDefencesList: string[] = [];
-    if (opponentDefences) {
-      opponentDefencesList = opponentDefences.map(item => `@${this.gameRoom.game.getPlayer(item.data.userId)?.name}`);
-    }
-    let characterNames: string[] = [];
-    if (characters) {
-      characterNames = characters.map(item => (item.data as CharacterData).characterName);
-    }
-    // const character = this.gameRoom.game.getPlayer(userId)?.character;
-    this.bot.telegram.sendMessage(
-      userId,
-      `Раунд: ${round}\n
-      @${username} (Очки: ${points})\n
-      Ходы: ${join(characterNames, ', ')}\n
-      Атаковал: ${attackPlayer}\n
-      Защитился: ${defencePlayer}\n
-      Кто атаковал: ${join(opponentAttacksList, ', ')}\n
-      Кто защитился: ${join(opponentDefencesList, ', ')}\n
-      `,
-      menuButtons,
-    ); // refresh
+    this.bot.telegram.sendMessage(userId, getStatus(this.gameRoom.game.getStatusData()), menuButtons); // refresh
   }
 }
